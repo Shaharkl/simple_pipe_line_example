be5f72296a1cb93f1226f42139a27d3d4712e596
Shaharkl <sk2601kl@gmail.com>
2020-08-03 12:13:35 +0300

    v.0.1

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 IFS=',' read -r -a array <<< "$1"
+if [ ${#array[@]} -eq 0 ] 
+then 
+    echo "No argument"
+    echo "There no use to run this script"
+    exit 1
+fi
 for element in "${array[@]}"
 do
-    git add "$element"
+    if [ ! -f "$element" ]  
+    then
+        echo "Error: $element file not found."
+        exit 2
+    else
+        git add "$element"
+    fi
 done
-git commit -m "commited All" 
-docker build . -t shaharkl/pipline:v.0.0  
-docker push shaharkl/pipline:v.0.0
+git commit -m "$2" 
+docker build . -t shaharkl/simple_pipeline:"$3"  
+docker push shaharkl/simple_pipeline:"$3"
